39dcf8d65deb5e477902fe801ce14f2a773a8503
antonio909 <78562889+antonio909@users.noreply.github.com>
2024-06-08 16:46:48 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link       http://www.arikaim.com
+ * @copyright  Copyright (c) Konstantin Atanasov <info@arikaim.com)>
+ * @license    http://www.arikaim.com/license
+ *
+*/
+
+include_once "vendor/autoload.php";
+// load config file
+$config = include_once "arikaim/config/config.php";
+
+Arikaim\Core\Arikaim::run(0,$config);
