e193801a8db4b27ac10d293822b6c631c8ecbd4e
antonio909 <78562889+antonio909@users.noreply.github.com>
2025-05-19 19:32:31 -0300

    Update config.php

--- arikaim/config/config.php
+++ arikaim/config/config.php
@@ -1,45 +1,45 @@
 <?php
 /**
- * Arikaim
- * @link      http://www.arikaim.com
- * @copyright Copyright (c) 2017-2019 Konstantin Atanasov <info@arikaim.com>
- * @license   http://www.arikaim.com/license
+ * My CMS
+ * @link
+ * @copyright
+ * @license
  */
 
 return [
     // application settings
     'settings' => [
-        'debug'           => false,
-        'debugTrace'      => true,
-        'jwtKey'          => '49ec7df6-ea4f-4364-bd59-1c6609b1840a',
-        'defaultLanguage' => 'en',
-        'cache'           => false,
-        'primaryTemplate' => 'blog',
-        'cacheDriver'     => 'void',
-        'logEvents'       => false,
-        'console'         => [
-            'log'       => false,
-            'logErrors' => false
-        ],
-        'timeZone'        => 'UTC',
-        'dateFormat'      => 'F j, Y',
-        'timeFormat'      => 'g:i:s A',
-        'numberFormat'    => 'accounting',
-        'logger'          => true,
-        'sessionInterval' => '2',
-        'loggerHandler'   => 'file'
+            'debug'           => false,
+            'debugTrace'      => true,
+            'jwtKey'          => '49ec7df6-ea4f-4364-bd59-1c6609b1840a',
+            'defaultLanguage' => 'en',
+            'cache'           => false,
+            'primaryTemplate' => 'blog',
+            'cacheDriver'     => 'void',
+            'logEvents'       => false,
+            'console'         => [
+                    'log'       => false,
+                    'logErrors' => false
+            ],
+            'timeZone'        => 'UTC',
+            'dateFormat'      => 'F j, Y',
+            'timeFormat'      => 'g:i:s A',
+            'numberFormat'    => 'accounting',
+            'logger'          => false,
+            'sessionInterval' => '2',
+            'loggerHandler'   => 'db'
     ],
     // database settings
     'db' => [
-        'database'    => 'arikaim',
-        'username'    => '',
-        'password'    => '',
-        'driver'      => 'mysql',
-        'host'        => 'localhost',
-        'charset'     => 'utf8',
-        'collation'   => 'utf8_bin',
-        'prefix'      => '',
-        'engine'      => 'InnoDB ROW_FORMAT=DYNAMIC'
+        'database'  => 'mycms',
+        'username'  => '',
+        'password'  => '',
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_bin',
+        'prefix'    => '',
+        'engine'    => 'InnoDB ROW_FORMAT=DYNAMIC'
     ],
     // middlewares
     'middleware' => [
@@ -49,3 +49,4 @@ return [
     'environment' => [
     ]
 ];
+                        
